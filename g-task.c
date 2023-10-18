5accbbbf5fdd0b60fa053034e1f613d0a9fc5828
MariamMahmoud28 <mm0376722@gmail.com>
2023-10-18 21:23:12 +0300

    l

---
+++ g-task.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+/**
+ * _getline_command - print "#cisfun$ "
+ * Return: line of string input for user
+ *
+ */
+
+char *_getline_command(void)
+{
+		char *lineptr = NULL;
+		size_t charter_user = 0;
+
+	if (isatty(STDIN_FILENO))
+	write(STDOUT_FILENO, "$ ", 2);
+
+	if (getline(lineptr, &charter_user, stdin) == -1)
+	{
+		free(lineptr);
+		return (NULL);
+	}
+
+			return (liner);
+}
