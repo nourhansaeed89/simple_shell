71c7b76e07fa8ea723f8b3899b820a87aec5742d
MariamMahmoud28 <mm0376722@gmail.com>
2023-10-18 21:41:42 +0300

    task

---
+++ A-task.c
@@ -0,0 +1,15 @@
+<p align="center">
+     <p align="center">
+               <img src="https://nullsector.co/wp-content/uploads/2017/12/Bash-new.sh_.png" width="360"/
+	</p>
+	p align="center">
+	<a href="https://github.com/ellerbrock/open-source-badges/">
+	<img alt="bash" src="https://badges.frapsoft.com/bash/v1/bash.png?v=103" target="_blank" />
+	</a>
+	<a href="https://github.com/RicardoArquimedes/simple_shell/commits/master">
+	<img alt="commit_activity" src="https://img.shields.io/github/commit-activity/y/RicardoArquimedes/simple_shell" target="_blank" />
+	</a>
+	<a href="https://github.com/RicardoArquimedes/simple_shell/graphs/contributors">
+	<img alt="contributors" src="https://img.shields.io/github/contributors/RicardoArquimedes/simple_shell" target="_blank" />
+	</a>
+
